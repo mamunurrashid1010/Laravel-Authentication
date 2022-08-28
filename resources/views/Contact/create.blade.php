d18b63af5c1adf4dc51fe931a34bb4c037d7f693
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-28 15:57:14 +0600

    Create create.blade.php to show contacts list

---
+++ resources/views/Contact/create.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Contact Form</div>
+
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('contact.save') }}">
+                            @csrf
+
+                            <div class="row mb-3">
+                                <label for="name" class="col-md-4 col-form-label text-md-end">Name</label>
+                                <div class="col-md-6">
+                                    <input id="name" type="text" class="form-control" name="name" value="" required>
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">Phone</label>
+                                <div class="col-md-6">
+                                    <input id="phone" type="text" class="form-control" name="phone" value="" required >
+                                </div>
+                            </div>
+
+                            <div class="row mb-3">
+                                <label for="email" class="col-md-4 col-form-label text-md-end">Address</label>
+                                <div class="col-md-6">
+                                    <input id="address" type="text" class="form-control" name="address" value="" required>
+                                </div>
+                            </div>
+                            <div class="row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">Save Now</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
