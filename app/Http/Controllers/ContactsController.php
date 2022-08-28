b090420591c6b529491cd2bf8cbdc8a86a729bdd
mamunurrashid1010 <96053862+mamunurrashid1010@users.noreply.github.com>
2022-08-28 16:00:22 +0600

    create create(), save(), show() function to view contact form, save contact data and view contact list

--- app/Http/Controllers/ContactsController.php
+++ app/Http/Controllers/ContactsController.php
@@ -2,9 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Contacts;
 use Illuminate\Http\Request;
 
 class ContactsController extends Controller
 {
-    //
+
+    /**
+     * create function return view with contact form
+     */
+    public function create(){
+        return view('Contact.create');
+    }
+
+    /**
+     * save function insert data to contacts table
+     */
+    public function save(Request $request){
+        $request->validate([
+            'name'  => 'required',
+            'phone'  => 'required',
+            'address'  => 'required',
+        ]);
+        $result=Contacts::insert([
+            'name'      => $request->name,
+            'phone'     => $request->phone,
+            'address'   => $request->address,
+        ]);
+        if($result)
+            return redirect()->to('/contact/show');
+        else
+            return ['message'=>'Error'];
+    }
+
+    /**
+     * show function return contacts list
+     */
+    public function show(){
+        $contacts=Contacts::all();
+        return $contacts;
+    }
 }
